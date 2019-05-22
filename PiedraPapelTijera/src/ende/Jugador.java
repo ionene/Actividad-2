c28b750748838203db89e809d57f9df7711ddb3f
ionene <ione8ibanez@gmail.com>
2019-05-22 12:15:35 +0200

    visibilidad variables clase jugador

--- PiedraPapelTijera/src/ende/Jugador.java
+++ PiedraPapelTijera/src/ende/Jugador.java
@@ -6,8 +6,8 @@ package ende;
 	 */
 public class Jugador{
 	   
-	    int exitos;      // número de partidas ganadas
-	    int winTotal;
+	    private int exitos;      // número de partidas ganadas
+	    private int winTotal;
 	    
 	    /**
 	     * Escoge piedra, papel o tijera al azar
